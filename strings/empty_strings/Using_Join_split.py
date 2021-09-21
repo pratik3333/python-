3dd8697dae7996fc2df628ad57c1c7ae6cf1e338
pratik kagale <pratikkagale3333@gmail.com>
2021-09-21 10:50:09 -0600

    Remove empty strings from list of strings Using join() + split()

---
+++ strings/empty_strings/Using_Join_split.py
@@ -0,0 +1,9 @@
+                  #Using join() + split()
+"""Combining both the join() and split() operations, this task can also be 
+achieved.We first join all the strings so that empty space is removed, and then 
+split it back to list so that new list made now has no empty string."""
+
+test_list=["","pratik","","","Thank You"]
+
+new_list=" ".join(test_list).split()
+print(new_list)

d8b6117fa314d1e7f7e7cd7f1c3b7e8bf934d8c7
pratik kagale <pratikkagale3333@gmail.com>
2021-09-21 11:07:00 -0600

    modified

--- strings/1.py
+++ strings/1.py
@@ -1,12 +1,16 @@
 """In Python, string is an immutable sequence data type.
 It is the sequence of Unicode characters wrapped inside single, double, or triple quotes."""
+#
+# str="Geeksforgeeks is fun"
+#
+# n=4
+# modified_str=''
+# for char in range(0,len(str)):
+#     if(char != n):
+#        modified_str +=str[char]
+#
+# print(modified_str)
 
-str="Geeksforgeeks is fun"
-
-n=4
-modified_str=''
-for char in range(0,len(str)):
-    if(char != n):
-       modified_str +=str[char]
-
-print(modified_str)
+test_list=["","","pratik"]
+new_list=list(filter(None,test_list))
+print(f"The modified list is {new_list}")

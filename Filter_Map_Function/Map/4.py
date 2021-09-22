c2b41f0614835312a25cbb4acc561d20ec900ea5
pratik kagale <pratikkagale3333@gmail.com>
2021-09-22 14:01:18 -0600

    added two list x1+x2 using map() and lambda function

---
+++ Filter_Map_Function/Map/4.py
@@ -0,0 +1,12 @@
+
+
+ #adding two list x1+x2
+
+number1=[1,2,3,4,5,6,7,8,9,5]
+number2=[2,8,5,22,55,4,3,2,1,4]
+
+n=list(map(lambda x1,x2 : x1+x2 ,number1,number2))
+print(n)
+
+#OP:-
+#[3, 10, 8, 26, 60, 10, 10, 10, 10, 9]

67f351f4a454411509b3efbd33e880755834d450
pratik kagale <pratikkagale3333@gmail.com>
2021-09-22 17:05:21 -0600

    finding the adults from given ages

--- Filter_Map_Function/Filter/3.py
+++ Filter_Map_Function/Filter/3.py
@@ -1,13 +1,17 @@
+# ages=[5, 6, 78, 23, 55, 21, 22, 20, 18, 14]
+#
+# def myfunc(x):
+#     if x > 18 :
+#         return True
+#     return False
+#
+#
+# adults = filter(myfunc,ages)
+#
+# for x in adults:
+#     print(x)
+#
+
 ages=[5, 6, 78, 23, 55, 21, 22, 20, 18, 14]
-
-def myfunc(x):
-    if x > 18 :
-        return True
-    return False
-
-
-adults = filter(myfunc,ages)
-
-for x in adults:
-    print(x)
-
+adults=filter(lambda x: x > 18, ages)
+print(list(adults))

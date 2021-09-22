138f0fc4d8f54f01b183b21aeb5c74f244a5cd5f
pratik kagale <pratikkagale3333@gmail.com>
2021-09-22 11:41:47 -0600

    finding the even and odd numbers from the given list

--- Filter/1.py
+++ Filter/1.py
@@ -1,7 +1,7 @@
 
     #Finding the even and odd numbers from the given list
 
-number=[23,54,65,77,87,99,78,66,88,44]
+number=[23,54,44,65,77,87,99,78,66,88,44]
 
 even_number=list(filter(lambda x : x%2 ==0,number))
 print(even_number)

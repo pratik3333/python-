95e0bda636e014a80366dcf8ab81c74e386a0a5a
pratik kagale <pratikkagale3333@gmail.com>
2021-09-22 14:20:52 -0600

    adding two list using map and lambda

---
+++ Filter_Map_Function/Map/5.py
@@ -0,0 +1,16 @@
+
+
+number1=[1,2,3,4]
+number2=[2,3,4,5]
+
+n=list(map(lambda x1,x2: x1+x2,number1,number2))
+print(n)
+
+print("\U0001F600")
+print("\U0001F637")
+
+
+# OP:-
+# [3, 5, 7, 9]
+# 😀
+# 😷

936e7b830f0a02ee52fd19a65110b97d3929b08c
pratik kagale <pratikkagale3333@gmail.com>
2021-09-23 13:04:35 -0600

    Converts the first character to upper case

--- Other/Join_.py
+++ Other/Join_.py
@@ -1,12 +1,13 @@
+#
+#
+#
+#
+# #Join all items in a dictionary into a string, using a the word "TEST" as separator:
+#
+# myDict = {"name": "John", "country": "Norway"}
+# mySeparator = "TEST"
+#
+# x = mySeparator.join(myDict)
+#
+# print(x)
 
-
-
-
-#Join all items in a dictionary into a string, using a the word "TEST" as separator:
-
-myDict = {"name": "John", "country": "Norway"}
-mySeparator = "TEST"
-
-x = mySeparator.join(myDict)
-
-print(x)
---
+++ strings/Capitalize/first_character.py
@@ -0,0 +1,8 @@
+
+#Converts the first character to upper case
+a='hey, man how are you'
+print(a.capitalize())
+
+
+# OP:-
+# Hey, man how are you

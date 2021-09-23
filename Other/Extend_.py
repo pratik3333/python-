16091f8dbf6fdd8f47de40b3843e7c673e58954d
pratik kagale <pratikkagale3333@gmail.com>
2021-09-23 14:35:55 -0600

    To append elements from another list to the current list, use the extend() method

---
+++ Other/Extend_.py
@@ -0,0 +1,13 @@
+
+                            #Extend List
+#To append elements from another list to the current list, use the extend() method.
+
+
+test_list=["hey","hello","how"]
+test_list2=["man","jack","you"]
+
+test_list.extend(test_list2)
+print(test_list)
+
+#OP:-
+#['hey', 'hello', 'how', 'man', 'jack', 'you']

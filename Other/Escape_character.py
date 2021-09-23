789f5a166a2e48a123b7ff2c6758ac7df2367dcb
pratik kagale <pratikkagale3333@gmail.com>
2021-09-23 12:46:13 -0600

    An escape character is a backslash \

---
+++ Other/Escape_character.py
@@ -0,0 +1,7 @@
+               #Escape Character
+"""To insert characters that are illegal in a string, use an escape character.
+An escape character is a backslash \ followed by the character you want to insert"""
+
+
+test_text="hey \"man\" how are you"
+print(test_text)

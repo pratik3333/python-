32c4a1e9470fcef8a289ebc899d08d827ac27928
pratik kagale <pratikkagale3333@gmail.com>
2021-09-23 15:15:12 -0600

    The pop() method removes the specified index.

---
+++ Other/Remove/pop_method.py
@@ -0,0 +1,13 @@
+
+
+# Remove Specified Index
+# The pop() method removes the specified index.
+
+
+test_list=["hey","man","how","are","you"]
+test_list.pop(2)
+print(test_list)
+
+#
+# OP:-
+# ['hey', 'man', 'are', 'you']

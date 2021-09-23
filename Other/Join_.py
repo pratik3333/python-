b70d3d65ac54dc7f3566e88963854c06b8e63fd4
pratik kagale <pratikkagale3333@gmail.com>
2021-09-23 12:56:52 -0600

    Join all items in a dictionary into a string, using a the word TEST as separator:

---
+++ Other/Join_.py
@@ -0,0 +1,12 @@
+
+
+
+
+#Join all items in a dictionary into a string, using a the word "TEST" as separator:
+
+myDict = {"name": "John", "country": "Norway"}
+mySeparator = "TEST"
+
+x = mySeparator.join(myDict)
+
+print(x)

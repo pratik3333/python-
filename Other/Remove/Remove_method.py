4e747d70add34fdd81e9717c18bf69bfcf8e9c44
pratik kagale <pratikkagale3333@gmail.com>
2021-09-23 15:10:50 -0600

    The remove() method removes the specified item

---
+++ Other/Remove/Remove_method.py
@@ -0,0 +1,11 @@
+
+# Remove Specified Item
+# The remove() method removes the specified item.
+
+test_list=["hey","man","where","are"]
+test_list.remove("man")
+print(test_list)
+
+
+# OP:-
+# ['hey', 'where', 'are']

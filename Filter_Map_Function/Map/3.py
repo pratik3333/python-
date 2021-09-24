fc10ee5821d01a0f6775f3f59b62c04e8cdc0234
pratik kagale <pratikkagale3333@gmail.com>
2021-09-24 12:39:26 -0600

    Double all numbers using map() and lambda

--- Filter_Map_Function/Map/3.py
+++ Filter_Map_Function/Map/3.py
@@ -5,3 +5,4 @@
 number=[1,2,3,4,5,6,7,8,9,88,88]
 s=list(map(lambda x: x+x ,number))
 print(s)
+
---
+++ practice/1.py
@@ -0,0 +1,26 @@
+#
+#
+# n=input("Enter the line:-")
+# n=n.title()
+# n=n.split()
+# string=""
+# for i in n:
+#     string+=i[:-1]+i[-1].upper()+" "
+# print(string)
+
+#
+# test_list=[1,2,3,4,55,66,76,86,78,65]
+# even_numbers=list(filter(lambda x:x%2 ==0,test_list))
+# print(f"The even numbers are:- {even_numbers}")
+#
+# odd_numbers=list(filter(lambda x:x%2 !=0,test_list))
+# print(f"The odd numbers are:- {odd_numbers}")
+
+
+#
+# n=["","hey","","man"]
+# new_list=list(filter(None,n))
+# print(new_list)
+
+
+

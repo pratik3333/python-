4698cefaba1eec59ae9c68c965235ee51a27d0b4
pratik kagale <pratikkagale3333@gmail.com>
2021-09-27 12:07:59 +0530

    Multilevel inheritance

---
+++ Inheritance/multilevel_inheritance.py
@@ -0,0 +1,37 @@
+          #Multilevel inheritance
+
+
+class Dad:
+    backetball=1
+
+class Son(Dad):
+    Dance=1
+    def isdance(self):
+        return f"Yes i dance {self.Dance} number of times."
+
+class Grandson(Son):
+    Dance = 6
+    backetball=78
+
+    def isdance(self):
+        return f"Jackson yeah! yes i dance very awesomely {self.Dance} number of times"
+
+class Grandson(Son):
+    Dance = 6
+    backetball=78
+
+    def isdance(self):
+        return f"Jackson yeah! yes i dance very awesomely {self.Dance} number of times"
+
+
+
+darry=Dad
+larry=Son
+harry=Grandson
+
+print(darry.backetball)
+print(larry.backetball)
+print(harry.backetball)
+
+print(harry.isdance(Son))
+print(larry.isdance(Grandson))

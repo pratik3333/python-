64a9932e229719bf41db0a5e28121ddc5bf2183d
pratik kagale <pratikkagale3333@gmail.com>
2021-09-30 11:09:54 +0530

    Finding the even and odd numbers from given list

--- Other/1.py
+++ Other/1.py
@@ -1,18 +1,14 @@
 
+#Find the even and odd numbers from the given list
 
-
-
-
-test_list1=[1,2,3,4,5,564,6,74,437,5,434]
+test_list=[1,2,3,4,34,664,7,767,9787,987,4554,864534,3,756,34,43,767,98]
 
 #even numbers
-even_numbers=list(filter(lambda x:x%2 ==0,test_list1))
-print(even_numbers)
+
+even_numbers=list(filter(lambda x:x%2 == 0,test_list))
+print(f"The even numbers are: {even_numbers}")
+
 
 #odd numbers
-odd_numbers=list(filter(lambda x:x%2 != 0,test_list1))
-print(odd_numbers)
-# 
-# OP:-
-# [2, 4, 564, 6, 74, 434]
-# [1, 3, 5, 437, 5]
+odd_numbers=list(filter(lambda x:x%2 != 0,test_list))
+print(f"The odd numbers are: {odd_numbers}")

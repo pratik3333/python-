542d97c751a0e8bb2237ff445fd7c0daa4723172
pratik kagale <pratikkagale3333@gmail.com>
2021-09-30 12:16:09 +0530

    Double all numbers using map() and lambda

---
+++ Other/4.py
@@ -0,0 +1,15 @@
+
+
+#
+# def add(n):
+#     return n+n
+# number=(12,34,564,23)
+# res=map(add,number)
+#
+# print(list(res))
+
+
+#Double all numbers using map() and lambda
+number=(12,34,564,23)
+Double_number=list(map(lambda x:x+x,number))
+print(Double_number)

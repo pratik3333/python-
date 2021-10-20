b6e8e4a46b665ba112fd1e649899b0485278d214
pratik kagale <pratikkagale3333@gmail.com>
2021-10-20 15:58:52 +0530

    addition of two numbers

--- Project/calc/views.py
+++ Project/calc/views.py
@@ -3,4 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html',{'name':'Pratik','age':21})
+    return render(request,'home.html',{'name':'Pratik'})
+
+def add(request):
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+
+    return render(request,"result.html",{'result':res})

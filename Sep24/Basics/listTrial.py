904b4876a1e6788d4de41474256f85030ac0d7e0
pratik kagale <pratikkagale3333@gmail.com>
2024-09-13 21:19:11 +0530

    list examples

--- Sep24/Basics/listTrial.py
+++ Sep24/Basics/listTrial.py
@@ -1,4 +1,11 @@
 
 courses=['history','math','physics','compsci']
 print(len(courses))
-print(courses)
+print(courses[-1:])
+
+nList=' - '.join(courses)
+
+new_courses=nList.split(' - ')
+print(nList) #separated by -
+print(new_courses) #back into original form
+

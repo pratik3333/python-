9bb5cf0af6628d3423826d23be1474d36b2a5de4
pratik kagale <pratikkagale3333@gmail.com>
2024-09-15 16:55:15 +0530

    reverse sorting

--- Sep24/Basics/sortedList.py
+++ Sep24/Basics/sortedList.py
@@ -2,6 +2,6 @@
 
 li=[23,34,1,53,24,12,35,55,3]
 
-s_li=sorted(li)
+s_li=sorted(li,reverse=True)
 
-print(f'before sorting list {li},\n after sorting list {s_li}')
+print(f'before sorting list {li},\n after reverse sorting list {s_li}')

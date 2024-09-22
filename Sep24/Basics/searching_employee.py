24c6873ef5b2fb10599f87b0331cc1d94b4d0b99
pratik kagale <pratikkagale3333@gmail.com>
2024-09-22 17:28:25 +0530

    searching employee from text file

---
+++ Sep24/Basics/searching_employee.py
@@ -0,0 +1,16 @@
+
+
+s_name=input("Enter searching name: ")
+d=1
+emp_file=open("employee_name.txt","r")
+
+for emp in emp_file.readline():
+    emp=emp.strip() #remove any leading/trailing white space
+    if (emp.lower()==s_name.lower()):
+        d=2
+        break
+if d==2:
+    print(f"Employee found {emp}")
+else:
+    print("Employee not found")
+

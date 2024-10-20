2352f21ef320cfad906caeb7cc8248c7085951e3
pratik kagale <pratikkagale3333@gmail.com>
2024-10-20 23:13:13 +0530

    added some files

---
+++ UI/JS/Basic/input.js
@@ -0,0 +1,12 @@
+
+let a=prompt("Please enter your age")
+
+a=Number.parseInt(a)  //converting string to number it is nothing but typecast
+
+if (a<18) {
+    
+    console.log("Invalid user")
+} else {
+    console.log("valid user")
+}
+// console.log(a);
